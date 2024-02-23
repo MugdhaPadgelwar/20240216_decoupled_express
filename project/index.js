475994287d36414ed04c874a68a52c8a797cda4a
MugdhaPadgelwar <101932627+MugdhaPadgelwar@users.noreply.github.com>
2024-02-23 10:47:39 +0530

    update files

--- project/index.js
+++ project/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = 3000;
+app.use(cors());
 
 const mongoose = require("mongoose");
 mongoose.connect(
